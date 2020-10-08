b1bedf60b003f4b1eb36638834438df825371801
Klezovich Anastasiya <nastyaklezovich@gmail.com>
2020-10-08 22:40:54 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return Number(n.toString().split("").slice(1, n.length).reverse().join(""));
+    } else {
+        return Number(n.toString().split("").reverse().join(""));
+    }
+};
